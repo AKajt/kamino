ad52bf1c6b02ee09892aa33cfb24eeecd4c15351
Unknown <kajtna.aljaz@gmail.com>
2018-08-13 20:10:30 +0200

    Pojos for api data

---
+++ app/src/main/java/com/aljazkajtna/kamino/data/pojo/Resident.java
@@ -0,0 +1,141 @@
+package com.aljazkajtna.kamino.data.pojo;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Resident {
+
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("height")
+    @Expose
+    private String height;
+    @SerializedName("mass")
+    @Expose
+    private String mass;
+    @SerializedName("hair_color")
+    @Expose
+    private String hairColor;
+    @SerializedName("skin_color")
+    @Expose
+    private String skinColor;
+    @SerializedName("eye_color")
+    @Expose
+    private String eyeColor;
+    @SerializedName("birth_year")
+    @Expose
+    private String birthYear;
+    @SerializedName("gender")
+    @Expose
+    private String gender;
+    @SerializedName("homeworld")
+    @Expose
+    private String homeworld;
+    @SerializedName("created")
+    @Expose
+    private String created;
+    @SerializedName("edited")
+    @Expose
+    private String edited;
+    @SerializedName("image_url")
+    @Expose
+    private String imageUrl;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getMass() {
+        return mass;
+    }
+
+    public void setMass(String mass) {
+        this.mass = mass;
+    }
+
+    public String getHairColor() {
+        return hairColor;
+    }
+
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+    }
+
+    public String getSkinColor() {
+        return skinColor;
+    }
+
+    public void setSkinColor(String skinColor) {
+        this.skinColor = skinColor;
+    }
+
+    public String getEyeColor() {
+        return eyeColor;
+    }
+
+    public void setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
+    }
+
+    public String getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(String birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getHomeworld() {
+        return homeworld;
+    }
+
+    public void setHomeworld(String homeworld) {
+        this.homeworld = homeworld;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public String getEdited() {
+        return edited;
+    }
+
+    public void setEdited(String edited) {
+        this.edited = edited;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+}
