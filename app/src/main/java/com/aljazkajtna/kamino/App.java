48d19f482f7fc1d69f97f373f5fa89c1218a44a2
AKajt <kajtna.aljaz@gmail.com>
2018-08-16 08:07:45 +0200

    DI for required classes

--- app/src/main/java/com/aljazkajtna/kamino/App.java
+++ app/src/main/java/com/aljazkajtna/kamino/App.java
@@ -1,4 +1,37 @@
 package com.aljazkajtna.kamino;
 
-public class App {
+import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
+
+import com.aljazkajtna.kamino.dagger.DaggerAppComponent;
+
+import javax.inject.Inject;
+
+import dagger.android.AndroidInjector;
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.HasActivityInjector;
+
+public class App extends Application implements HasActivityInjector {
+
+    @Inject
+    DispatchingAndroidInjector<Activity> dispatchingAndroidInjector;
+
+    public static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        this.initDagger();
+        context = getApplicationContext();
+    }
+
+    @Override
+    public AndroidInjector<Activity> activityInjector() {
+        return dispatchingAndroidInjector;
+    }
+
+    private void initDagger() {
+        DaggerAppComponent.builder().application(this).build().inject(this);
+    }
 }
--- app/src/main/java/com/aljazkajtna/kamino/ui/MainActivity.java
+++ app/src/main/java/com/aljazkajtna/kamino/ui/MainActivity.java
@@ -1,15 +1,44 @@
 package com.aljazkajtna.kamino.ui;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.aljazkajtna.kamino.R;
+import com.aljazkajtna.kamino.ui.planet.PlanetFragment;
 
-public class MainActivity extends AppCompatActivity {
+import javax.inject.Inject;
+
+import dagger.android.AndroidInjection;
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.support.HasSupportFragmentInjector;
+
+public class MainActivity extends AppCompatActivity implements HasSupportFragmentInjector {
+
+    @Inject
+    DispatchingAndroidInjector<Fragment> dispatchingAndroidInjector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AndroidInjection.inject(this);
+        loadFragment(savedInstanceState);
+    }
+
+    @Override
+    public DispatchingAndroidInjector<Fragment> supportFragmentInjector() {
+        return dispatchingAndroidInjector;
+    }
+
+    private void loadFragment(Bundle savedInstanceState) {
+        if (savedInstanceState == null) {
+            PlanetFragment fragment = new PlanetFragment();
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, fragment, null)
+                    .commit();
+        }
     }
 }
